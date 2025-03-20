881dcdfd4fff94aa8895a6c8db257fa7df4001ca
Pragalva <pragalvad@gmail.com>
2025-03-20 14:24:04 +0100

    changed agent to work with the state class

--- Hanabi/Agent.py
+++ Hanabi/Agent.py
@@ -1,6 +1,7 @@
 from Card import Card
 from Card import Color
 from typing import List
+from State_Andrea import State
 #Definiton of out agent
 #Our Agent will have a hand which we will use to generate what the other players can see
 class Agent:
@@ -20,12 +21,14 @@ class Agent:
     def get_cards_in_hand(self):
         return self.card_in_hand # Return the hand of the player
     
-    def get_visible_cards(self):
-        return self.cards_visible #Returns the visible hand of the agent
+    def get_visible_cards(self, current_state: "State"):
+        temp_hands = current_state.hands
+        temp_hands.pop[self.player_index]
+        return temp_hands #Returns the visible hand of the agent
     #######################################################################
     #########################################################################
     #Set funtions
-    def set_visible_cards(self, agent1: "Agent", agent2: "Agent") -> None:
+    ''';def set_visible_cards(self, agent1: "Agent", agent2: "Agent") -> None:
         #Updates the agent's cards_visible with the hands of two other agents.
         self.cards_visible = agent1.card_in_hand + agent2.card_in_hand  # Combine hands
     def set_color_hint(self,hint_index: int, hint: int):
@@ -38,55 +41,64 @@ class Agent:
         #Updates the agent's pending number hint list
         self.hint_number_pending[hint_index] = hint
         card_for_hint = self.card_in_hand[hint_index]
-        card_for_hint.set_number_hint(hint)
+        card_for_hint.set_number_hint(hint)'''
     ############################################################################
 
     ###############################################################################
     #Actions
     #Function to draw a card
-    def draw_card(self, deck = List[Card]):
-        if(deck and (len(self.card_in_hand)<= 5)): # Check if the agent can draw the card
-            new_card = deck.pop(0)  # Remove the first card from the deck
-            self.card_in_hand.append(new_card)  # Add it to the player's hand
+    def draw_card(self, current_state: "State"):
+        if(current_state.Game_Deck != 0 and (len(current_state.hands[self.player_index])<= 5)): # Check if the agent can draw the card
+            new_card = current_state.Game_Deck.pop(0)  # Remove the first card from the deck
+            current_state.hands[self.player_index].append(new_card)
+
+            '''self.card_in_hand.append(new_card)  # Add it to the player's hand
             self.hint_color.append(0) #Add a blank hint for new card
-            self.hint_number.append(0) #Add a blank hint for new card
+            self.hint_number.append(0) #Add a blank hint for new card'''
     
     #Function to discard card
-    def discard_card(self, deck = List[Card],discard =List[Card], discard_index =int, hints =int ):
-        discard_card = self.card_in_hand.pop(discard_index) #Remove the discard card from the hand
-        self.hint_number.pop(discard_index) #Discard the hint assosicated with the card
-        self.hint_color.pop(discard_index)
-        discard.append(discard_card) #add the discard card to the discard pile
-        self.draw_card(deck) #draw a new card
-        if(hints <=8):
-            hints = hints +1 #adds the token to the
+    def discard_card(self, current_state: "State", discard_index =int ):
+        discard_card = current_state.hands[self.player_index].pop(discard_index) #Remove the discard card from the hand
+        '''self.hint_number.pop(discard_index) #Discard the hint assosicated with the card
+        self.hint_color.pop(discard_index)'''
+
+        current_state.Discard_pile.append(discard_card) #add the discard card to the discard pile
+        self.draw_card(current_state.Game_Deck) #draw a new card
+        if(current_state.Total_hints <=8):
+            current_state.Total_hints = current_state.Total_hints +1 #adds the token to the
+
     #Function to play a card
-    def play_card(self, deck = List[Card],play_pile = List[Card],discard_pile = List[Card],play_index =int, Fuse =int, Board_pile =List[Card]):
-        Card_play = self.card_in_hand.pop(play_index) #Remove the play card from hand
-        self.hint_number.pop(play_index) #Discard the hint assosicated with the card
-        self.hint_color.pop(play_index)
-        for i in play_pile:
+    def play_card(self, current_state: "State", play_index =int):
+        Card_play = current_state.hands[self.player_index].pop(play_index) #Remove the play card from hand
+
+        '''self.hint_number.pop(play_index) #Discard the hint assosicated with the card
+        self.hint_color.pop(play_index)'''
+        for i in current_state.Play_pile:
             if i.get_color() == Card_play.get_color() : #Loop through to find the right color
                 if (i.get_number()+1) == Card_play.get_number(): #Check if the card if playable
                     i.number = Card_play.get_number() #Change the number to the current playable card.
-                    Board_pile.append(i)#Add the card to list of cards played
+                    current_state.Board_pile.append(i)#Add the card to list of cards played
                 else:
-                    Fuse = Fuse -1 #Lose a life for losing a life
+                    current_state.Fuse_Token = current_state.Fuse_Token -1 #Lose a life for losing a life
                     print(f"Oh no!!, you lost a life.")
-                    discard_pile.append(Card_play) #Add the card to the discard pile
+                    current_state.Discard_pile.append(Card_play) #Add the card to the discard pile
         
-        self.draw_card(deck)#Draw a card form the deck
+        self.draw_card(current_state)#Draw a card form the deck
+
     #Function to give hint
-    def give_hint(self, hint_agent :"Agent", hint_type = int, hint_index = int, hint = int, hint_tokens = int):
-        if(hint_tokens > 0 ): #Check if you have hints left
+    def give_hint(self, current_state: "State", hint_type = int, hint_index = int, hint = int, player_hint_index = int):
+        if(current_state.Total_hints > 0 ): #Check if you have hints left
             #For color hint
             if hint_type == 0:
-                Agent.set_color_hint(hint_index,hint) #Call the set funtion to give color hint
+                #Agent.set_color_hint(hint_index,hint) #Call the set funtion to give color hint
+                current_state.hands[player_hint_index][hint_index].set_color_hint(hint)
+
             #For number hint
             elif hint_type == 1:
-                Agent.set_number_hint(hint_index,hint) #Call the set function to give number hint
+                #Agent.set_number_hint(hint_index,hint) #Call the set function to give number hint
+                current_state.hands[player_hint_index][hint_index].set_number_hint(hint)
             
-            hint_tokens = hint_tokens -1 #Decrease the hint total
+            current_state.Total_hints = current_state.Total_hints -1 #Decrease the hint total
         else:
             print("Sorry you have no hints left")
     ###################################################################################
