e06950e770d996b7a9b170b870b1fbb1872b0698
Leona-Vo <s250471@dtu.dk>
2025-03-20 15:38:06 +0100

    Updated decision and probability matrix

--- Hanabi/Decision.py
+++ Hanabi/Decision.py
@@ -1,4 +1,4 @@
-from Eval import eval_discard, playable
+from Eval import eval_discard, playable, eval_play
 from State import State
 from Agent import Agent
 from Prob import probability_matrix
@@ -8,12 +8,13 @@ def Decision_for_action(current_state:"State",current_agent:"Agent", lifes_left:
     Player_2 = Agent('Player_2',2)
 
     best_discard_index = eval_discard(current_agent,current_state.Discard_pile,current_state.Board_pile)
-    best_play_index= 1
-    best_hint_index, hinted_agent = (1, Player_2)
+    best_play_index= eval_play(current_agent, current_state)
+    best_hint_index, hint_type, hinted_agent = (1, Player_2)
 
 
-    current_probability_tensor = probability_matrix(current_agent,current_state.Discard_pile,current_state.Board_pile)
-    hint_probability_tensor = probability_matrix(hinted_agent,current_state.Discard_pile,current_state.Board_pile)
+    current_probability_tensor = probability_matrix(current_agent,current_state,2,0)
+    hint_probability_tensor = probability_matrix(hinted_agent,current_state,2,0)
+    after_hint_probability_tensor = probability_matrix(hinted_agent,current_state,hint_type, best_hint_index)
     expected_outcome = [0,0,0]
 
     #Checking for best play
@@ -27,7 +28,7 @@ def Decision_for_action(current_state:"State",current_agent:"Agent", lifes_left:
     #Checking for best hint
     for i in range(5):
         for j in range(5):
-            expected_outcome[1] += hint_probability_tensor[i,j,best_hint_index]*playable(hinted_agent.card_in_hand[best_hint_index], current_state.Play_pile)
+            expected_outcome[1] += (after_hint_probability_tensor[i,j,best_hint_index] - hint_probability_tensor[i,j,best_hint_index])*playable(hinted_agent.card_in_hand[best_hint_index], current_state.Play_pile)
 
     #Checking for best discard
     for i in range(5):
--- Hanabi/Prob.py
+++ Hanabi/Prob.py
@@ -1,10 +1,11 @@
-import Agent
+from Agent import Agent
+from State_Andrea import State
 from Card import Color
 import numpy as np
 
-def probability_matrix (player:"Agent", discard_pile, board_pile):
+def probability_matrix (player:"Agent", state:"State", hint_type, hint_index):
 
-    cards_seen = player.cards_visible + discard_pile + board_pile
+    cards_seen = player.get_visible_card(state) + state.Discard_pile + state.Board_pile
     cards_left = 50 - len(cards_seen) 
     
     cards_of_a_color = 10
@@ -12,29 +13,40 @@ def probability_matrix (player:"Agent", discard_pile, board_pile):
     cards_in_game = [1,1,1,2,2,3,3,4,4,5]
     
     prop_matrix = np.zeros((5, 5, 5))
+    post_hint_number = state.hands[player.player_index].get_number_hint()
+    post_hint_color = state.hands[player.player_index].get_color_hint()
+
+    if hint_type == 0: #color
+        for i in hint_index:
+            card_hint = state.hands[player.player_index[i]]
+            post_hint_color[i] = card_hint.get_color()  
+    elif hint_type == 1: #number
+        for i in hint_index:
+            card_hint = state.hands[player.player_index[i]]
+            post_hint_number[i] = card_hint.get_number()  
 
     for i in range(len(player.card_in_hand)):
-        if (player.hint_color[i] != 0) & (player.hint_number[i] != 0): #we got hints about that exact card
-            prop_matrix[player.hint_color[i] - 1, player.hint_number[i]-1, i] = 1 # we are certain, that it is that card
+        if (post_hint_color[i] != 0) & (post_hint_number[i] != 0): #we got hints about that exact card
+            prop_matrix[post_hint_color[i] - 1, post_hint_number[i]-1, i] = 1 # we are certain, that it is that card
 
-        elif (player.hint_color[i] != 0) & (player.hint_number[i] == 0): #we know just the color
-            cards_color_seen = sum(1 for card in cards_seen if card.color == player.hint_color[i])
+        elif (post_hint_color[i] != 0) & (post_hint_number[i] == 0): #we know just the color
+            cards_color_seen = sum(1 for card in cards_seen if card.get_color() == post_hint_color[i])
             for numbers in range(5):
-                this_card_seen = sum(1 for card in cards_seen if (card.color == player.hint_color[i]) & (card.number == numbers + 1))
+                this_card_seen = sum(1 for card in cards_seen if (card.get_color() == post_hint_color[i]) & (card.get_number() == numbers + 1))
                 n_tot = sum(np.sum(cards_in_game == numbers + 1))
-                prop_matrix[player.hint_color[i] - 1, numbers, i] = (n_tot - this_card_seen)/(cards_of_a_color - cards_color_seen)
+                prop_matrix[post_hint_color[i] - 1, numbers, i] = (n_tot - this_card_seen)/(cards_of_a_color - cards_color_seen)
 
-        elif (player.hint_color[i] == 0) & (player.hint_number[i] != 0): #we just know the number
-            cards_number_seen = sum(1 for card in board_pile if card.number == player.hint_number[i])
+        elif (post_hint_color[i] == 0) & (post_hint_number[i] != 0): #we just know the number
+            cards_number_seen = sum(1 for card in state.Board_pile if card.get_number() == post_hint_number[i])
             for colors in Color:
-                this_card_seen = sum(1 for card in cards_seen if (card.color == colors) & (card.number == player.hint_number[i]))
-                n_tot = sum(np.sum(cards_in_game == player.hint_number[i]))
-                prop_matrix[colors - 1, player.hint_number[i] - 1, i] = (n_tot - this_card_seen)/(n_tot*5 - cards_number_seen)
+                this_card_seen = sum(1 for card in cards_seen if (card.get_color() == colors) & (card.get_number() == post_hint_number[i]))
+                n_tot = sum(np.sum(cards_in_game == post_hint_number[i]))
+                prop_matrix[colors - 1, post_hint_number[i] - 1, i] = (n_tot - this_card_seen)/(n_tot*5 - cards_number_seen)
 
-        elif (player.hint_color[i] == 0) & (player.hint_number[i] == 0): #we don't know anything about the card
+        elif (post_hint_color[i] == 0) & (post_hint_number[i] == 0): #we don't know anything about the card
             for numbers in range(5):
                 for colors in Color:
-                    this_card_seen = sum(1 for card in cards_seen if (card.color == colors) & (card.number == numbers + 1))
+                    this_card_seen = sum(1 for card in cards_seen if (card.get_color() == colors) & (card.get_number() == numbers + 1))
                     n_tot = sum(np.sum(cards_in_game == numbers + 1))
                     prop_matrix[colors - 1, numbers, i] = (n_tot - this_card_seen)/(cards_left)
         
